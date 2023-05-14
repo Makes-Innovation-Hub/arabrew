06a7366dccaeb35a83a2fb51361c7185da6208ca
T1To48 <tofek.98@gmail.com>
2023-05-14 22:48:21 +0300

    pretiier and removing  unneccasary comment out

--- server/routes.js
+++ server/routes.js
@@ -4,13 +4,11 @@ import chatRouter from "./api/chat/chat.routes.js";
 import messageRouter from "./api/message/message.routes.js";
 import translationRouter from "./api/translation/translation.routes.js";
 
+const router = express.Router();
 
-const router=express.Router();
-
-router.use("/user",userRouter)
-router.use("/chat",chatRouter)
-router.use("/message",messageRouter)
-router.use("/translation",translationRouter)
-
+router.use("/user", userRouter);
+router.use("/chat", chatRouter);
+router.use("/message", messageRouter);
+router.use("/translation", translationRouter);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -8,20 +8,17 @@ import errorHandler from "./middleware/errorHandler.js";
 
 import allRoutes from "./routes.js";
 
-dotenv.config({ path:"/home/code-grinder/Documents/Makes Internship may-juni-23/Projects/Arabrew-chatapp/arabrew-repo/server/config/config.env" });
+dotenv.config({
+  path: "/home/code-grinder/Documents/Makes Internship may-juni-23/Projects/Arabrew-chatapp/arabrew-repo/server/config/config.env",
+});
 
 connectDB();
-
-
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// const logger=pino({
-//   level:process.env.NODE_ENV!=="production"&&"info",
-// })
-if(process.env.NODE_ENV!=="production"){
+if (process.env.NODE_ENV !== "production") {
   app.use(pino);
 }
 
@@ -29,15 +26,20 @@ app.get("/", (req, res) => {
   res.send("Server is ACTIVE  ");
 });
 
- app.use("/arabrew/v1",allRoutes)
+app.use("/arabrew/v1", allRoutes);
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server=app.listen(PORT,console.log(` ⭐⭐server is running in ${process.env.NODE_ENV} Mode, & made on port ${PORT} ⭐⭐`))
+const server = app.listen(
+  PORT,
+  console.log(
+    ` ⭐⭐server is running in ${process.env.NODE_ENV} Mode, & made on port ${PORT} ⭐⭐`
+  )
+);
 
-process.on('unhandledRejection',(err,promise)=>{
-    console.log(`😡😡 Error: ${err.message} 😡😡`);
-    server.close(()=>process.exit(1))
-})
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`😡😡 Error: ${err.message} 😡😡`);
+  server.close(() => process.exit(1));
+});
