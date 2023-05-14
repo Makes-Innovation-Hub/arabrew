4217b15915897994576d2874fd0dc0b61baec227
Digvorzhak <shon.h777@gmail.com>
2023-05-14 16:21:49 +0300

    basic structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import {store} from './store.jsx'
+import { Provider } from "react-redux";
+import ErrorBoundary from "./ErrorBoundary.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ErrorBoundary fallback="Error">
+    <Provider store={store}>
     <App />
+    </Provider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
