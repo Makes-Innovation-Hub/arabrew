738d798f329c650a4cd3c918fd1f0aa62d81f332
Digvorzhak <shon.h777@gmail.com>
2023-05-14 20:21:24 +0300

    fixed react-error-boundery

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
+"use client"
+
+import { ErrorBoundary } from "react-error-boundary";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import {store} from './app/store.jsx'
 import { Provider } from "react-redux";
-import ErrorBoundary from "./ErrorBoundary.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary fallback="Error">
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
     <Provider store={store}>
     <App />
     </Provider>
