a5ddfedfd9a94089f0e3452198316acfe07621a9
Digvorzhak <shon.h777@gmail.com>
2023-05-15 22:08:41 +0300

    fixed dom errors with svg file

--- client/src/assets/Glass.jsx
+++ client/src/assets/Glass.jsx
@@ -3,7 +3,7 @@
 export default function Glass() {
   
     return (
-<svg class="feather feather-coffee" fill="none" height="125" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="125" xmlns="http://www.w3.org/2000/svg"><path d="M18 8h1a4 4 0 0 1 0 8h-1"/><path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"/><line x1="6" x2="6" y1="1" y2="4"/><line x1="10" x2="10" y1="1" y2="4"/><line x1="14" x2="14" y1="1" y2="4"/></svg>
+<svg className="feather feather-coffee" fill="none" height="125" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" idth="2" viewBox="0 0 24 24" width="125" xmlns="http://www.w3.org/2000/svg"><path d="M18 8h1a4 4 0 0 1 0 8h-1"/><path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"/><line x1="6" x2="6" y1="1" y2="4"/><line x1="10" x2="10" y1="1" y2="4"/><line x1="14" x2="14" y1="1" y2="4"/></svg>
     
     )
   }
