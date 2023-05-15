99521f3d1576a94795efa1ea1a261f72aaf755de
Saleh Kalouti <salehkalouti@gmail.com>
2023-05-15 23:55:32 +0300

    removed unwanted lines from chatdisplayarea

--- client/src/components/Chat/ChatDisplayArea/ChatDisplayArea.jsx
+++ client/src/components/Chat/ChatDisplayArea/ChatDisplayArea.jsx
@@ -1,21 +1,13 @@
 import chats from "../InputArea/chatsDummyDataGenerator.js";
-import React, { useEffect, useRef } from "react";
 import { ChatsContainer } from "./ChatDisplayAreaStyles.jsx";
 import MessageBox from "../MessageBox/MessageBox.jsx";
 
 export default function ChatDisplayArea() {
-  const chatPageRef = useRef(null);
   return (
     <ChatsContainer>
-      <div
-        className="chat-page"
-        ref={chatPageRef}
-        style={{ display: "flex", flexDirection: "column" }}
-      >
-        {chats.map((message, index) => (
-          <MessageBox message={message} key={index} />
-        ))}
-      </div>
+      {chats.map((message, index) => (
+        <MessageBox message={message} key={index} />
+      ))}
     </ChatsContainer>
   );
 }
