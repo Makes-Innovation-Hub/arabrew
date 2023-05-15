40fe7437eae07fcfb34941d83a383acd0332bb4a
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-05-15 17:10:17 +0300

    19-add-occupation-page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
+import { ErrorBoundary } from "react-error-boundary";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import { store } from "./app/store.jsx";
+import { Provider } from "react-redux";
+import Error from "./components/Error.jsx";
+import GlobalStyles from "./components/styles/GlobalStyles.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <ErrorBoundary fallback={<Error />}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
