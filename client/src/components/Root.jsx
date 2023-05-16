8923c413e6e36f55e4ad9b3078d7fcd19b46fc79
mika <mikagivner123@gmail.com>
2023-05-16 14:53:19 +0300

    finely-third push-25 ex

--- client/src/components/Root.jsx
+++ client/src/components/Root.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
-import HumburgerSvg from "../assets/humburger.jsx"
+import HamburgerSvg from "../assets/hamburger.jsx"
 import LogoSvg from "../assets/logo";
 export default function RootLayout() {
   return (
     <>
-      <Header leftIcon={<HumburgerSvg/>} midIcon={<LogoSvg/>} rightIcon={""}/>
+      <Header leftIcon={<HamburgerSvg/>} midIcon={<LogoSvg/>} rightIcon={""}/>
       <Outlet />
     </>
   );
