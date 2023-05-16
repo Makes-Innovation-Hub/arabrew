80e9bcfeecddb2e2565de3c3db2670e089dace83
Saleh Kalouti <salehkalouti@gmail.com>
2023-05-16 13:47:43 +0300

    other comments

--- client/src/components/PageLayout/PageLayout.jsx
+++ client/src/components/PageLayout/PageLayout.jsx
@@ -11,12 +11,14 @@ export default function PageLayout() {
   return (
     <BackLayout>
       <HeaderWrapper>
+        {/* do not remove this div even if it is empty */}
         <div style={{ width: "20%" }}>
           <img src={arrowIcon} />
         </div>
         <TitleWrapper>
           <PageTitle>Add Age</PageTitle>
         </TitleWrapper>
+        {/* do not remove this div even if it is empty */}
         <div style={{ width: "20%" }}>
           {/*  here you can add code for additional elements in the header */}
         </div>
