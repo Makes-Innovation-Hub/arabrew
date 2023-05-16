34bc7dc29cab0a4243b63931cfe93bc9901b7d7f
T1To48 <tofek.98@gmail.com>
2023-05-17 01:07:32 +0300

    add .jsx to all import's src

--- client/src/pages/Intro.jsx
+++ client/src/pages/Intro.jsx
@@ -1,21 +1,19 @@
-import { ThemeProvider } from "styled-components"
-import Glass from "../assets/Glass"
-import { Flex } from "../components/styles/Flex"
+import { ThemeProvider } from "styled-components";
+import Glass from "../assets/Glass.jsx";
+import { Flex } from "../components/styles/Flex.jsx";
 
 const theme = {
-  colors: {
-
-  },
-  mobile: "798px"
-}
+  colors: {},
+  mobile: "798px",
+};
 
 const Intro = () => {
   return (
     <ThemeProvider theme={theme}>
-    <Flex>
-      <Glass/>
-    </Flex>
+      <Flex>
+        <Glass />
+      </Flex>
     </ThemeProvider>
-  )
-}
-export default Intro
+  );
+};
+export default Intro;
