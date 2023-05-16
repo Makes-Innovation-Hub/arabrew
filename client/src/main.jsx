eff9ac03c4f38d7799d12011a17fc0202cb54010
yishain11 <yishain11@walla.co.il>
2023-05-16 18:55:51 +0300

    added react import in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { ErrorBoundary } from "react-error-boundary";
 import ReactDOM from "react-dom/client";
+import React from "react";
 import "./index.css";
 import App from "./App.jsx";
 import { store } from "./app/store.jsx";
@@ -9,12 +10,12 @@ import GlobalStyles from "./styles/GlobalStyles.jsx";
 import Error from "./components/Error.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-      <GlobalStyles />
-      <ErrorBoundary fallback={<Error />}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ErrorBoundary>
-    </React.StrictMode> 
+  <React.StrictMode>
+    <GlobalStyles />
+    <ErrorBoundary fallback={<Error />}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
+  </React.StrictMode>
 );
