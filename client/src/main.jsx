1db1f344dd5736a33152f07ba56615f2c8a7cebc
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-05-17 09:14:54 +0300

    rtk query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { store } from "./app/store.jsx";
 import { Provider } from "react-redux";
-import { ApiProvider } from "@reduxjs/toolkit/query/react";
-import { userDataApi } from "./features/userDataApi.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ApiProvider api={userDataApi}>
-        <App />
-      </ApiProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
