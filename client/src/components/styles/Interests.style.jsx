cb32d18561d284b3b7dbd558550c4cd3e833d51b
T1To48 <tofek.98@gmail.com>
2023-05-17 05:22:03 +0300

    interest page styling

---
+++ client/src/components/styles/Interests.style.jsx
@@ -0,0 +1,30 @@
+export const selectedBtn = {
+  color: "white",
+  fontSize: "0.9rem",
+  backgroundColor: "#50924e",
+  borderRadius: "4rem",
+  border: "none",
+  padding: "1rem",
+  margin: "0 0.3rem",
+};
+
+export const listBtn = {
+  fontSize: "0.9rem",
+  backgroundColor: "#EDF2F7",
+  borderRadius: "4rem",
+  border: "none",
+  padding: "1rem",
+  margin: "0.3rem 0.3rem",
+};
+
+export const selectedContainer = {
+  fontSize: "1rem",
+  padding: "0.4rem",
+  border: "none",
+  margin: "1rem",
+  fontWeight: "500",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  lineHeight: "1.6rem",
+};
