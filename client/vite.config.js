e9b2678104335fe43d8e26e65fc247353389657d
T1To48 <tofek.98@gmail.com>
2023-05-17 06:53:36 +0300

    add field to access .env files

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    env: {
+      VITE_WEB_SOCKET_PORT: process.env.VITE_WEB_SOCKET_PORT,
+    },
+  },
+  server: {
+    host: true,
+  },
 });
