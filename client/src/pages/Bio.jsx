e7bd0b60da5b3f4a847eeaaa334b1c1a90800a25
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-05-17 14:30:45 +0300

    delete logs

--- client/src/pages/Bio.jsx
+++ client/src/pages/Bio.jsx
@@ -1,31 +1,14 @@
 import { useSelector } from "react-redux";
-// import { useLazySendUserDataMutation } from "../features/userDataApi";
 import { useSendUserDataMutation } from "../features/userDataApi";
 import { Button } from "../components/styles/Button";
 
 const Bio = () => {
   const userData = useSelector((state) => state.userData);
-
   const [sendUserData] = useSendUserDataMutation();
 
   const handleClick = async () => {
     sendUserData(userData);
-    console.log(userData);
   };
-
-  // const userData = useSelector((state) => state.userData);
-  // const [sendUserData] = useLazySendUserDataMutation();
-  // const [trigger] = useLazySendUserDataMutation();
-
-  // const handleClick = async () => {
-  //   // await trigger(userData);
-  //   sendUserData(userData);
-  //   console.log(userData);
-  // };
-
-  // if (isError)   return <h1>{error}</h1>;
-  // if (isLoading) return <h1>Loading...</h1>;
-  // if (isSuccess) return <h1>Success!</h1>;
 
   return (
     <>
