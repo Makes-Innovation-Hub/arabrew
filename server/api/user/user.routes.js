32b2987d8ff52afcad4b0bc435b5bc5fa8940d6e
T1To48 <tofek.98@gmail.com>
2023-05-19 13:30:24 +0300

    USERSChema DONE, registerUser done

--- server/api/user/user.routes.js
+++ server/api/user/user.routes.js
@@ -1,3 +1,7 @@
 import express from "express";
+import { registerUser } from "./user.controllers.js";
 const router = express.Router();
+
+router.route("/").post(registerUser);
+
 export default router;
