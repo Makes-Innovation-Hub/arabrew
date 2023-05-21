c470836a9d776bc005e3c9f98e02b7df903fae61
T1To48 <tofek.98@gmail.com>
2023-05-22 01:00:25 +0300

    char routes for the controller funcs

--- server/api/chat/chat.routes.js
+++ server/api/chat/chat.routes.js
@@ -1,3 +1,8 @@
 import express from "express";
+import { createChat, getUserChats } from "./chat.controllers.js";
 const router = express.Router();
+
+router.route("/all-chats/:userId").get(getUserChats);
+router.route("/new/:senderId/:friendId").post(createChat);
+
 export default router;
