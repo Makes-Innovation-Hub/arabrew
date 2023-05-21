cee25aa749fd2548cfa5bebf73871c9955af20b1
Digvorzhak <shon.h777@gmail.com>
2023-05-21 12:21:18 +0300

    header render rotation changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,16 @@
-import { Outlet } from "react-router-dom";
-import HeaderLayout from "./HeaderLayout.jsx";
+import { StyledHeader } from "../styles/StyledHeader";
+import { StyledHeaderTitle } from "../styles/StyledHeaderTitle";
 
 
 
 export default function Header({ leftIcon, midIcon, rightIcon, title }) {
   return (
     <div>
-      <HeaderLayout
-        leftIcon={leftIcon}
-        midIcon={midIcon}
-        title={title}
-        rightIcon={rightIcon}
-      />
+        <StyledHeader>
+        <div>{leftIcon}</div>
+        <div>{midIcon ? midIcon : <StyledHeaderTitle>{title}</StyledHeaderTitle>}</div>
+        <div>{rightIcon}</div>
+      </StyledHeader>
     </div>
   );
 }
--- client/src/components/HeaderLayout.jsx
+++ client/src/components/HeaderLayout.jsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { StyledHeader } from "../styles/StyledHeader";
-import { StyledHeaderTitle } from "../styles/StyledHeaderTitle";
-export default function HeaderLayout({ leftIcon, midIcon, rightIcon, title }) {
+
+
+
+export default function HeaderLayout() {
   return (
     <div>
-      <StyledHeader>
-        <div>{leftIcon}</div>
-        <div>{midIcon ? midIcon : <StyledHeaderTitle>{title}</StyledHeaderTitle>}</div>
-        <div>{rightIcon}</div>
-      </StyledHeader>
+      <StyledHeader/>
       <Outlet />
     </div>
   );
