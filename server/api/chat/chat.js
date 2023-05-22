4c856f5bbf1ffd6919db600ec9c27d1d59f47fbf
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-05-22 14:00:46 +0300

    in progress

--- server/api/chat/chat.js
+++ server/api/chat/chat.js
@@ -1,1 +1,34 @@
-//test
+import mongoose from "mongoose";
+
+const ChatSchema = new mongoose.Schema(
+  {
+    userNames: {
+      type: String,
+      required: true,
+    },
+    messages: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Message",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+    toObject: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+export default mongoose.model("Chat", ChatSchema);
--- server/api/message/message.js
+++ server/api/message/message.js
@@ -1,1 +1,44 @@
-//test
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema(
+  {
+    from: {
+      type: String,
+      required: true,
+    },
+    to: {
+      type: String,
+      required: true,
+    },
+    timeStamp: {
+      type: Date,
+      default: Date.now,
+    },
+    arMsg: {
+      type: String,
+      required: true,
+    },
+    heMsg: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+    toObject: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+export default mongoose.model("Chat", ChatSchema);
