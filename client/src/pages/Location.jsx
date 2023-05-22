37c1ac7e025f54a0a60f0e7bd3bdc5ee1c308396
Digvorzhak <shon.h777@gmail.com>
2023-05-22 14:15:42 +0300

    initial commit

--- client/src/pages/Location.jsx
+++ client/src/pages/Location.jsx
@@ -1,6 +1,25 @@
+import { useDispatch } from "react-redux"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { Header } from "../components"
+import { Flex, StyledPage, StyledMargin, StyledButton, StyledPageTitle } from "../styles"
+import { ArrowLeft } from "../assets"
+import { addUserDataField } from "../features/userDataSlice"
+
+
+
 const Location = () => {
   return (
-    <div>Location</div>
+    <div>
+      <Header leftIcon={<Link to="/gender"><ArrowLeft/></Link>} title={"Add Location"} />
+      <StyledPage>
+        <StyledMargin direction="vertical" margin="1.75rem"/>
+            <StyledMargin direction="horizontal" margin="35rem">
+            <StyledPageTitle>Add your Location</StyledPageTitle>
+            </StyledMargin>
+            <StyledMargin direction="vertical" margin="9.25rem"/>
+      </StyledPage>
+    </div>
   )
 }
 export default Location
