e21f8c7f32b60c96c840fdfcaca2cb58b8679dca
mika <mikagivner123@gmail.com>
2023-05-23 20:34:14 +0300

    prettier code

--- client/src/pages/ConversationPage.jsx
+++ client/src/pages/ConversationPage.jsx
@@ -1,40 +1,65 @@
-import { Link } from "react-router-dom"
-import Header from "../components/Header"
-import {Flex, StyledPage, StyledMargin,NoConversationStyle,ContentConversationPage,ConversationPageStyle,ButtonConversation,ConversationNoData,ChatsDisplay,ButtonForChats,BlockDiv} from "../styles"
-import { SmallGlass, Hamburger } from "../assets"
-import ConversationDisplay from "../components/ConversationDisplay"
-import { Button } from "../styles/StyledButton"
-
-  const ConversationPage = ({prevConversation}) => {
-    return (
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import {
+  Flex,
+  StyledPage,
+  StyledMargin,
+  NoConversationStyle,
+  ContentConversationPage,
+  ConversationPageStyle,
+  ButtonConversation,
+  ConversationNoData,
+  ChatsDisplay,
+  ButtonForChats,
+  BlockDiv,
+} from "../styles";
+import { SmallGlass, Hamburger } from "../assets";
+import ConversationDisplay from "../components/ConversationDisplay";
+import { Button } from "../styles/StyledButton";
+const ConversationPage = ({ prevConversation }) => {
+  return (
     <div>
       <StyledMargin direction="vertical" margin="5%">
-      <Header leftIcon={<Link to="/"><Hamburger/></Link>} title={<SmallGlass/>} />
+        <Header
+          leftIcon={
+            <Link to="/">
+              <Hamburger />
+            </Link>
+          }
+          title={<SmallGlass />}
+        />
       </StyledMargin>
-        <StyledPage>
-            
-                {prevConversation.length!==0?
-                <ConversationNoData><div>Conversation</div>
-                <ChatsDisplay>
-                {prevConversation.map((chat, i)=>{
-                    return < ConversationDisplay key={i} nameCon={chat.name} contentCon={chat.lastCon} profile={chat.profile}/>})}
-                    </ChatsDisplay>
-                    <BlockDiv/>
-                    <ButtonForChats>Search for friends to chat</ButtonForChats>
-                    </ConversationNoData>
-                    :<ConversationPageStyle><NoConversationStyle>No Conversation</NoConversationStyle> 
-                    <ContentConversationPage>Add some friends and start chatting with them, Your conversations will show up here.</ContentConversationPage>
-          <ButtonConversation>Search for friends to chat</ButtonConversation></ConversationPageStyle>}
-               
-                
-                
-                
-                
-          
-
-        </StyledPage>
-
+      <StyledPage>
+        {prevConversation.length !== 0 ? (
+          <ConversationNoData>
+            <div>Conversation</div>
+            <ChatsDisplay>
+              {prevConversation.map((chat, i) => {
+                return (
+                  <ConversationDisplay
+                    key={i}
+                    nameCon={chat.name}
+                    contentCon={chat.lastCon}
+                    profile={chat.profile}
+                  />
+                );
+              })}
+            </ChatsDisplay>
+            <BlockDiv />
+            <ButtonForChats>Search for friends to chat</ButtonForChats>
+          </ConversationNoData>
+        ) : (
+          <ConversationPageStyle>
+            <NoConversationStyle>No Conversation</NoConversationStyle>
+            <ContentConversationPage>
+              Add some friends and start chatting with them, Your conversations
+              will show up here.
+            </ContentConversationPage>
+            <ButtonConversation>Search for friends to chat</ButtonConversation>
+          </ConversationPageStyle>
+        )}
+      </StyledPage>
     </div>
-    )
-  }
-  export default ConversationPage
+  );
+};
+export default ConversationPage;
