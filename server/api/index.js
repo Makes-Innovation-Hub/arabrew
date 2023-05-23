942cb13f712e9881abea01634c19c8cd2aa1d53f
T1To48 <tofek.98@gmail.com>
2023-05-23 20:47:57 +0300

    index done

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,9 @@
-import asyncHandler from "../middleware/asyncHandler.js";
 import { log } from "../helpers/logger.js";
+
+import asyncHandler from "../middleware/asyncHandler.js";
+
 import User from "./user/user.js";
 import Chat from "./chat/chat.js";
 import Message from "./message/message.js";
-export { asyncHandler, User, Chat, Message, log };
+
+export { log, asyncHandler, User, Chat, Message };
