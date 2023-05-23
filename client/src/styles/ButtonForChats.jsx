abc36745f1b53583615a7b4057383f3cdfb07934
mika <mikagivner123@gmail.com>
2023-05-23 22:09:58 +0300

    dry-buttons

--- client/src/styles/ButtonForChats.jsx
+++ client/src/styles/ButtonForChats.jsx
@@ -1,31 +1,25 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-  export const ButtonForChats = styled.button`
-    border-radius: 0.6rem;
-    border: none;
-    width: 34rem;
-    height: 5rem;
-    padding: 1rem 0.5rem 1rem 0.5rem;
-    cursor: pointer;
-    background-color: ${({bg}) => bg || "#50924E"};
-    color: #fff;
-    font-style: italic;
-    transition: background-color 0.3s;
-    position: fixed;
-    left: 50%;
-    transform: translateX(-50%);
-    bottom: 4rem;
-    z-index: 2;
+export const ButtonForChats = styled.button`
+  border-radius: 0.6rem;
+  border: none;
+  width: 34rem;
+  height: 5rem;
+  padding: 1rem 0.5rem 1rem 0.5rem;
+  cursor: pointer;
+  background-color: ${({ bg }) => bg || "#50924E"};
+  color: #fff;
+  font-style: italic;
+  transition: background-color 0.3s;
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 4rem;
+  z-index: 2;
 
-    &:hover{
-      background-color: ${({hoverBg}) => hoverBg || "#396d37"};
-    };`
-const StyledButton = ({ to, text, ...rest }) => {
-  return (
-    <Link to={to}>
-      <Button {...rest}>{text}</Button>
-    </Link>
-  );
-};
+  &:hover {
+    background-color: ${({ hoverBg }) => hoverBg || "#396d37"};
+  }
+`;
 export default StyledButton;
