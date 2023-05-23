fb614704c58a7606c5c74fe0bc1e6ffd1f50cbe0
Saleh Kalouti <salehkalouti@gmail.com>
2023-05-23 16:12:09 +0300

    checks

--- client/src/pages/BioPage/BioPage.jsx
+++ client/src/pages/BioPage/BioPage.jsx
@@ -81,6 +81,7 @@ export default function BioPage() {
             <StyledSaveAndNextButton
               onClick={() => {
                 dispatch(addUserDataField(text));
+                console.log(userData);
                 sendUserData(userData);
               }}
             >
