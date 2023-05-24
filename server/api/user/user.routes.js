3f49143373986d193250a9c09bfd5a773a34699b
T1To48 <tofek.98@gmail.com>
2023-05-24 05:20:04 +0300

    user endpoints,

--- server/api/user/user.routes.js
+++ server/api/user/user.routes.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { getUserBySubId, registerUser } from "./user.controllers.js";
+import { filterByInterests } from "./user.middleware.js";
+import {
+  registerUser,
+  getUserBySubId,
+  getUsersByInterests,
+  getAllUsers,
+} from "./user.controllers.js";
 const router = express.Router();
+router.use(express.urlencoded({ extended: true }));
 
 router.route("/").post(registerUser);
 router.route("/:subId").get(getUserBySubId);
-
+router
+  .route("/find-users/:userId")
+  .get(filterByInterests, getUsersByInterests, getAllUsers);
 export default router;
