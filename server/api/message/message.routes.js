a83cc43d170c24de67ebee26c9161c25a22cdae3
T1To48 <tofek.98@gmail.com>
2023-05-24 05:12:14 +0300

    messages endpoints with global authorization

--- server/api/message/message.routes.js
+++ server/api/message/message.routes.js
@@ -1,3 +1,12 @@
 import express from "express";
+import { getMessagesByChatId, sendMessage } from "./message.controllers.js";
+import { isUserInChat } from "./message.middlewares.js";
 const router = express.Router();
+
+router.param("userId", isUserInChat);
+
+router
+  .route("/:chatId/:userId")
+  .post(sendMessage)
+  .get(isUserInChat, getMessagesByChatId);
 export default router;
