1f8f1c73173d723603ef7e7f9eaff0f063cd8f97
T1To48 <tofek.98@gmail.com>
2023-05-28 02:45:49 +0300

    Not DEBUGGED!!!, I did IT

--- client/src/pages/GenderSelection.jsx
+++ client/src/pages/GenderSelection.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Header } from "../components";
+import Header from "../components.jsx";
 import {
   Flex,
   StyledPage,
