af2cad6f8f7f52b5ebfa215e6d3f9e73363e9cf5
yishain11 <yishain11@walla.co.il>
2023-05-27 22:46:17 +0300

    fixed bug

--- scripts/generateConfig.sh
+++ scripts/generateConfig.sh
@@ -7,9 +7,9 @@ read -p "OPEN_AI_API_KEY: " open_ai_api_key
 
 touch ./server/.env
 echo '
-  PORT='$port',
-  NODE_ENV='$node_env',
-  WEB_SOCKET_PORT='$web_socket_port',
-  MONGO_URI='$mongo_uri',
+  PORT='$port'
+  NODE_ENV='$node_env'
+  WEB_SOCKET_PORT='$web_socket_port'
+  MONGO_URI='$mongo_uri'
   OPEN_AI_API_KEY='$open_ai_api_key'
 ' > ./server/.env
