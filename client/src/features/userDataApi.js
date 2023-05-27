d41ae32f373388a177f44ac98b017db77c7599d5
T1To48 <tofek.98@gmail.com>
2023-05-28 02:41:22 +0300

    !! important comment out !!

--- client/src/features/userDataApi.js
+++ client/src/features/userDataApi.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+//! this API slice is for all of our https Requests not just the userData...
 const userDataApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5005" }),
   endpoints: (builder) => ({
