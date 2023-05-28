5fde1ac31d44484b4221ee2cb62aee907dc2c23f
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-05-28 20:15:27 +0300

    working progress

--- client/src/features/userDataApi.js
+++ client/src/features/userDataApi.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const userDataApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5005" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5050" }),
   endpoints: (builder) => ({
     sendUserData: builder.mutation({
       query: (userData) => ({
-        url: "/user-data",
+        url: "/api/user/user-data",
         method: "POST",
         body: userData,
       }),
