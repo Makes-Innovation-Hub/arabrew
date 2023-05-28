0c86298e21ac8aeefe240cdb6fdf2f304ccf2fe0
yishain11 <yishain11@walla.co.il>
2023-05-28 21:47:13 +0300

    fixed path to dist dir

--- server/server.js
+++ server/server.js
@@ -44,11 +44,10 @@ wss.on("connection", (ws) => {
 
 app.use("/api", routes);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 const PORT = process.env.PORT || 5050;
 
