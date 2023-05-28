b8f3e415c1d68676027561fe2d23d9391390b8ea
T1To48 <tofek.98@gmail.com>
2023-05-28 21:57:34 +0300

    added the corret route for the chatsLIST

--- server/api/chat/chat.routes.js
+++ server/api/chat/chat.routes.js
@@ -4,6 +4,7 @@ import {
   addMessageToChat,
   createChat,
   getChatByNames,
+  getUserChats,
 } from "./chat.controllers.js";
 
 const router = express.Router();
@@ -13,5 +14,7 @@ router
   .post(createChat)
   .get(deleteOldMessages, getChatByNames)
   .put(addMessageToChat);
+
+router.route("/logged/:loggedUser_name/").get(getUserChats);
 
 export default router;
