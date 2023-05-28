6cfe17ccc2dd8791a91b9b7b37f44b8720e8a584
Saleh Kalouti <salehkalouti@gmail.com>
2023-05-28 23:38:50 +0300

    added lazy load

--- client/src/styles/NationalityPage/CountriesCustomOPtions.jsx
+++ client/src/styles/NationalityPage/CountriesCustomOPtions.jsx
@@ -1,25 +1,30 @@
 import { components } from "react-select";
 import { Flex } from "../Flex";
+import LazyLoad from "react-lazy-load";
+import React from "react";
 
-const Option = (props) => {
+const Option = React.memo((props) => {
   const { label, value, data } = props;
   return (
-    <components.Option {...props}>
-      <Flex style={{ justifyContent: "flex-start", width: "100%" }}>
-        <img
-          src={data.image}
-          alt={label}
-          style={{
-            marginRight: "0.5rem",
-            width: "3.125rem",
-            borderRadius: "2rem",
-            margin: "0 1rem 0 0",
-          }}
-        />
-        {label}
-      </Flex>
-    </components.Option>
+    <LazyLoad height={30} offset={300}>
+      <components.Option {...props}>
+        <Flex style={{ justifyContent: "flex-start", width: "100%" }}>
+          <img
+            loading="lazy"
+            src={data.image}
+            alt={label}
+            style={{
+              marginRight: "0.5rem",
+              width: "3.125rem",
+              borderRadius: "2rem",
+              margin: "0 1rem 0 0",
+            }}
+          />
+          {label}
+        </Flex>
+      </components.Option>
+    </LazyLoad>
   );
-};
+});
 
 export default Option;
