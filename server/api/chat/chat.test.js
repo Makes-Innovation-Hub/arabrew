195cdeb8bc53e68fe91ec0c5279e366f9731eefe
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-05-28 09:21:24 +0300

    backend tests

--- server/api/chat/chat.test.js
+++ server/api/chat/chat.test.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 
 describe("save message in db test", () => {
   it("should return a 200 status code", async function (done) {
-    this.timeout(50000);
+    this.timeout(5000);
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -19,7 +19,7 @@ describe("save message in db test", () => {
     };
 
     const res = await fetch(
-      "http://localhost:5000/api/chat/6469d8f248e79b74fce5a7aa/6469fe15a7083dcee2aa6908",
+      "http://localhost:5050/api/chat/6469d8f248e79b74fce5a7aa/6469fe15a7083dcee2aa6908",
       requestOptions
     );
     console.log("res", res);
