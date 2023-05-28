70063be2879d3856d001d10d12c0379453e5c835
T1To48 <tofek.98@gmail.com>
2023-05-28 21:13:37 +0300

    get by interests and gell functions

--- server/api/user/user.controllers.js
+++ server/api/user/user.controllers.js
@@ -1,6 +1,10 @@
 import asyncHandler from "../../middleware/asyncHandler.js";
 import User from "./user.js";
 
+Array.prototype.sortByMatching = function () {
+  return this.sort((a, b) => b.sortBy - a.sortBy);
+};
+
 export const registerUser = asyncHandler(async (req, res, next) => {
   const userInfo = req.body;
   const newUser = await User.create(userInfo);
@@ -10,5 +14,62 @@ export const registerUser = asyncHandler(async (req, res, next) => {
   return res.status(200).json({
     success: true,
     data: newUser,
+  });
+});
+
+export const getUsersByInterests = asyncHandler(async (req, res, next) => {
+  if (!req.interests) return next();
+  const { subId } = req.params;
+  const interests = req.interests || null;
+  const usersInterests = interests
+    ? { "userDetails.interests": { $in: interests } }
+    : {};
+  const matchingUsers = await User.find({
+    subId: { $ne: subId },
+    ...usersInterests,
+  }).lean();
+  if (!matchingUsers || matchingUsers.length < 1) return next();
+
+  let sorted_matchingUsers = matchingUsers
+    .map((user) => {
+      const { userDetails } = user;
+      const matching_interests_number = interests.reduce((total, current) => {
+        return userDetails.interests.includes(current)
+          ? (total += 1)
+          : total + 0;
+      }, 0);
+
+      return {
+        sortBy: matching_interests_number,
+        ...user,
+      };
+    })
+    .sortByMatching();
+  sorted_matchingUsers.forEach((user) => {
+    delete user.sortBy;
+    delete user._id;
+  });
+  res.status(200).json({
+    success: true,
+    data: sorted_matchingUsers,
+  });
+});
+
+export const getAllUsers = asyncHandler(async (req, res, next) => {
+  console.log("its me again&&");
+  const { subId } = req.params;
+  let allUsers = await User.find({
+    subId: { $ne: subId },
+  }).lean();
+  if (!allUsers || allUsers.length < 1)
+    return next(new Error("Db is Empty, or minor Error"));
+  allUsers = allUsers.map((user) => {
+    delete user._id;
+    return user;
+  });
+  allUsers = allUsers.sort(() => Math.random() - 0.5);
+  res.status(200).json({
+    success: true,
+    data: allUsers,
   });
 });
