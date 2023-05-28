7d6187c17d67583e11dd2d2b7ed23c07deb2adb0
Digvorzhak <shon.h777@gmail.com>
2023-05-28 19:59:37 +0300

    52-general-scripts

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.use("/api", routes);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
