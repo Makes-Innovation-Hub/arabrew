7f6dd93a7b821870b16402cde7fd1eaeb690a8b8
T1To48 <tofek.98@gmail.com>
2023-06-02 12:21:53 +0300

    added messages props

--- client/src/components/Chat/ChatDisplayArea/ChatDisplayArea.jsx
+++ client/src/components/Chat/ChatDisplayArea/ChatDisplayArea.jsx
@@ -1,14 +1,10 @@
-import chats from "../InputArea/chatsDummyDataGenerator.js";
 import MessageBox from "../MessageBox/MessageBox.jsx";
 import { ChatsContainer } from "../../../styles/Chat/ChatDisplay/ChatsContainer.jsx";
 
-export default function ChatDisplayArea() {
-  //! must be changed with the  the user name from the logged user slice #68-BRANCH
-  const userName = "taufiq zayyad";
-  //!  ////////////////////////////////////////////////////////////////////////////
+export default function ChatDisplayArea({ messages }) {
   return (
     <ChatsContainer>
-      {chats.map((message, index) => (
+      {messages.map((message, index) => (
         <MessageBox message={message} key={`${message},${index}`} />
       ))}
     </ChatsContainer>
