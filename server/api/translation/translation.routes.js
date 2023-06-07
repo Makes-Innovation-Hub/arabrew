a3d408a0553bc30599968172d50e88b0008e3a47
Saleh Kalouti <salehkalouti@gmail.com>
2023-06-07 14:27:42 +0300

    removed unwanted imports

--- server/api/translation/translation.routes.js
+++ server/api/translation/translation.routes.js
@@ -1,15 +1,7 @@
 import express from "express";
 import { messageController } from "./translation.controllers.js";
-import pino from "pino";
 
 const router = express.Router();
-
-const logger = pino();
-router.use((req, res, next) => {
-  const { url, method } = req;
-  logger.info(`Incoming request - URL: api/chat${url}, Method: ${method}`);
-  next();
-});
 
 router
   .route("/:user1/:user2/:original_lang/:target_lang")
