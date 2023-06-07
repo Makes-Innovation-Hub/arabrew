7f3fb71b441e49e7e9752cafb29981106f2b5966
Saleh Kalouti <salehkalouti@gmail.com>
2023-06-07 14:14:48 +0300

    commit

--- server/api/user/user.routes.js
+++ server/api/user/user.routes.js
@@ -5,17 +5,9 @@ import {
   getUsersByInterests,
   getAllUsers,
 } from "./user.controllers.js";
-import pino from "pino";
 
 const router = express.Router();
 
-const logger = pino();
-router.use((req, res, next) => {
-  const { url, method } = req;
-  logger.info(`Incoming request - URL: api/user${url}, Method: ${method}`);
-
-  next();
-});
 router.route("/register").post(registerUser);
 router
   .route("/:subId/get-users")
