01ea5c6b0621d9537e7dcbd015e59e59a72fe8d5
MrBennySolomon <MrBennySolomon@gmail.com>
2023-06-13 11:57:41 +0300

    in progress

--- client/src/components/Chat/ChatDisplayArea/ChatDisplayArea.jsx
+++ client/src/components/Chat/ChatDisplayArea/ChatDisplayArea.jsx
@@ -1,16 +1,26 @@
 import chats from "../InputArea/chatsDummyDataGenerator.js";
 import MessageBox from "../MessageBox/MessageBox.jsx";
 import { ChatsContainer } from "../../../styles/Chat/ChatDisplay/ChatsContainer.jsx";
+import { useState } from "react";
 
 export default function ChatDisplayArea() {
+  const [isPopupDisplaying, setIsPopupDisplaying] = useState(
+    chats.length === 0 ? true : false
+  );
+
+  const clickHandler = () => {
+    setIsPopupDisplaying(false);
+  };
+
   return (
     <ChatsContainer>
       {chats.map((message, index) => (
         // could be sorted by timestamp before display -not added here-
         <MessageBox message={message} key={index} />
       ))}
-      {chats.length === 0 && (
+      {isPopupDisplaying && (
         <img
+          onClick={clickHandler}
           width="200rem"
           height="200rem"
           src="../../../../img/click-me.jpg"
