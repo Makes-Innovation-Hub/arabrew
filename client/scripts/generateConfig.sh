9f01fb6a0b8954b76be202a9561a395b27611dbd
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-06-15 00:22:02 +0300

    fix env

--- client/scripts/generateConfig.sh
+++ client/scripts/generateConfig.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 read -p "VITE_WEB_SOCKET_PORT: " ws_port
 read -p "VITE_WEB_SOCKET_BASE_URL: " ws_base_url
-read -p "SERVER_BASE_URL: " server_base_url
-read -p "SERVER_PORT: " server_port
+read -p "VITE_SERVER_BASE_URL: " server_base_url
+read -p "VITE_SERVER_PORT: " server_port
 
 touch ./server/.env
 echo '
   VITE_WEB_SOCKET_PORT='$ws_port'
   VITE_WEB_SOCKET_BASE_URL='$ws_base_url'
-  SERVER_BASE_URL='$server_base_url'
-  SERVER_BASE_URL='$server_port'
+  VITE_SERVER_BASE_URL='$server_base_url'
+  VITE_SERVER_PORT='$server_port'
 ' > ./.env
