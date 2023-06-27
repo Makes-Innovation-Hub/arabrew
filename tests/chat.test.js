71ed53472b9b8c407d56422144a3e48039386391
Digvorzhak <shon.h777@gmail.com>
2023-06-27 19:44:10 +0300

    task

--- tests/chat.test.js
+++ tests/chat.test.js
@@ -13,47 +13,20 @@ dotenv.config({ path: __dirname + "../.env" });
 const PORT = process.env.PORT;
 const userId = Math.floor(Math.random() * 1000 + 1000);
 
-describe("chat creations tests", function () {
-  this.timeout(10000);
-  describe("save chat in db test", () => {
-    it("should return a 201 status code", async function () {
-      const myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
+describe("get user from db test", () => {
+  it("should GET the specific user by subId", async function () {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-      // const body = {
-      //   subId: userId,
-      //   name: "JohnDoe17",
-      //   avatar: "123",
-      //   userDetails: {
-      //     nativeLanguage: "HE",
-      //     interests: [
-      //       "swimming",
-      //       "programming",
-      //       "sleeping",
-      //       "playing",
-      //       "resting",
-      //     ],
-      //     yearOfBirth: "2000",
-      //     nationality: "Israel",
-      //     address: "Haifa",
-      //     gender: "Male",
-      //     occupation: "Doctor",
-      //     bio: "I Love People",
-      //   },
-      //   friends: ["A", "B", "C"],
-      // };
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+    };
 
-      const requestOptions = {
-        method: "POST",
-        // headers: myHeaders,
-        // body: JSON.stringify(body),
-      };
-
-      const res = await fetch(
-        `http://localhost:${PORT}/api/user/user-data`,
-        requestOptions
-      );
-      assert.strictEqual(res.status, 201);
-    });
+    const res = await fetch(
+      `http://localhost:${PORT}/api/chat/logged/user/${userId}`,
+      requestOptions
+    );
+    assert.equal(res.status, 200);
   });
 });
