d7169f381bdb649ed365544f6dc59d3d6486c08f
yishain11 <yishain11@walla.co.il>
2023-06-29 17:06:32 +0300

    used session storage to work with popup

--- client/src/components/Chat/ChatDisplayArea/ChatDisplayArea.jsx
+++ client/src/components/Chat/ChatDisplayArea/ChatDisplayArea.jsx
@@ -1,6 +1,6 @@
 import MessageBox from "../MessageBox/MessageBox.jsx";
 import { ChatsContainer } from "../../../styles/Chat/ChatDisplay/ChatsContainer.jsx";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { UserContext } from "../../../contexts/loggedUser.context.jsx";
 import { useState } from "react";
 import { useSelector } from "react-redux";
@@ -9,14 +9,20 @@ import PopupBubble from "../../../assets/PopupBubble.jsx";
 function ChatDisplayArea({ messages }) {
   const { userData: contextUser } = useContext(UserContext);
   const [isPopupDisplaying, setIsPopupDisplaying] = useState(() => {
-    if (messages.length === 0) {
+    const currentIsFirst = sessionStorage.getItem("isFirst");
+    if (currentIsFirst === undefined) {
+      sessionStorage.setItem("isFirst", true);
       return true;
     }
-    return false;
+    if (currentIsFirst === "false") {
+      return false;
+    }
+    return true;
   });
   const { name: loggedUser } = useSelector((state) => state.userRegister);
   const clickHandler = () => {
     setIsPopupDisplaying(false);
+    sessionStorage.setItem("isFirst", false);
   };
 
   return (
