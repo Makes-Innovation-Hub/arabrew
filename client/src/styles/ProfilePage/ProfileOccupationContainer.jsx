7aa89661aec6749beb577d006584af6e7d8aacac
zaid-kh <z.kh2800@gmail.com>
2024-02-19 13:01:58 +0200

    jihad's change

--- client/src/styles/ProfilePage/ProfileOccupationContainer.jsx
+++ client/src/styles/ProfilePage/ProfileOccupationContainer.jsx
@@ -5,7 +5,7 @@ export const ProfileOccupationContainer = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  border-bottom: 0.2rem solid lightgray;
+  border-bottom: 0.1rem solid lightgray;
   margin-bottom: 1.5vh;
   padding-bottom: 1.5vh;
 `;
