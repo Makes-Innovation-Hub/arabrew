535ed5829d8c0e73646cba07d5b6b30b87d9f97e
JihadMuna <jojo.muna.2019@gmail.com>
2024-02-19 12:34:36 +0200

    create a generic profile page

--- client/src/assets/LanguageIcon.jsx
+++ client/src/assets/LanguageIcon.jsx
@@ -1,13 +1,13 @@
 export default function LanguageIcon({ letter }) {
   return (
     <svg
-      width="32"
-      height="32"
+      width="30"
+      height="30"
       viewBox="0 0 33 33"
       fill=""
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx="16.5" cy="16.5" r="16.5" fill="#D9D9D9" />
+      <circle cx="16" cy="16" r="16.5" fill="#D9D9D9" />
       <text
         x="50%"
         y="50%"
