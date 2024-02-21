53b3e2a037acc5db6a04cc7a0bd4cd3eccd19dbf
jameel Agbaria <agbariajameel@gmail.com>
2024-02-21 15:19:53 +0200

    added update meetup functionality and route and added description for job routes

--- server/routes/job.routes.js
+++ server/routes/job.routes.js
@@ -12,12 +12,25 @@ import { validateToken } from "../middleware/verifyUserToken.js";
 
 const router = express.Router();
 
+// GET ALL JOB POSTS
 router.get("/", getAllJobs);
+
+// CREATE A JOB
 router.post("/", validateToken, createJob);
+
+// APPLY TO JOB
 router.patch("/apply", applyToJob);
+
+// GET ALL MY JOB POSTS
 router.get("/my-job-posts/", validateToken, getUserJobPosts);
+
+// GET JOB BY JOB-ID
 router.get("/:jobId", getJob);
+
+// UPDATE JOB BY JOB-ID
 router.patch("/:jobId", validateToken, updateJob);
+
+// DELETE JOB BY JOB-ID
 router.delete("/:jobId", validateToken, deleteJob);
 
 // add protected routes to create, get my posts , update details and delete
