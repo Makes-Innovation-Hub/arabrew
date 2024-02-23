287c0c363337e84daa1c18b01c1ebfd07acb17b6
Shhady Serhan <100072103+shhady@users.noreply.github.com>
2024-02-23 12:26:22 +0200

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,7 +12,7 @@ import { UserProvider } from "./contexts/loggedUser.context.jsx";
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-const baseClientUrl = `${import.meta.env.VITE_SERVER_BASE_URL}:5173`;
+const baseClientUrl = import.meta.env.VITE_DEVELOPMENT_MODE === 'dev' ? `${import.meta.env.VITE_CLIENT_BASE_URL}` :`${import.meta.env.VITE_SERVER_BASE_URL}:5173`;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
