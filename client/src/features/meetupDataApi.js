5bf012901d4a2f120c5375db1de91a1465aae6b8
Shhady Serhan <100072103+shhady@users.noreply.github.com>
2024-03-18 00:40:42 +0200

    Update meetupDataApi.js

--- client/src/features/meetupDataApi.js
+++ client/src/features/meetupDataApi.js
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = import.meta.env.VITE_SERVER_BASE_URL;
-const port = import.meta.env.VITE_SERVER_PORT;
+
 
 const meetupDataApi = createApi({
   reducerPath: "meetupDataApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${baseUrl}:${port}/api`,
+    baseUrl: `${baseUrl}/api`,
     tagTypes: ["Meetup"],
   }),
   endpoints: (builder) => ({
